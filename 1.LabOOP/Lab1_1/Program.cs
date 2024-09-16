d55aa4c91dba2985087e19467f69195af970c9f6
Bibub <slappierR@gmail.com>
2024-09-16 16:39:00 +0500

    lab 1, tasks 1-2 mostly done

---
+++ 1.LabOOP/Lab1_1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 0;
+            int m = 0;
+            bool nIsInt = false;
+            bool mIsInt = false;
+            //Ввод и проверка на целочисленность переменных n,m
+            do
+            {
+                try 
+                {
+                    Console.WriteLine("Enter n (int):");
+                    nIsInt = int.TryParse(Console.ReadLine(), out n);
+                    Console.WriteLine("Enter m (int):");
+                    mIsInt = int.TryParse(Console.ReadLine(), out m);
+                    if (!nIsInt || !mIsInt) throw new FormatException($"Types do not match\n n:{nIsInt} \n m:{mIsInt}\n");
+                }
+                catch(FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch 
+                {
+                    Console.WriteLine("Unknown error!");
+                }
+            }
+            while (!nIsInt || !mIsInt);
+
+            //Вывод результатов с переменными n,m
+            Console.WriteLine($"Values (n) : (m) are {n} : {m}");
+            Console.WriteLine($"1)n++ *m: {n++ *m}");
+            Console.WriteLine($"Values (n) : (m) are {n} : {m}");
+            Console.WriteLine($"2)m-- <n: {m-- <n}");
+            Console.WriteLine($"Values (n) : (m) are {n} : {m}");
+            Console.WriteLine($"3)++m >n: {++m >n}");
+            Console.WriteLine($"Values (n) : (m) are {n} : {m}");
+
+
+            double x = 0.0;
+            var xIsDouble = false;
+            //Ввод и проверка на тип double переменных x
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter x (double): ");
+                    xIsDouble = double.TryParse(Console.ReadLine(), out x);
+                    if (!xIsDouble) throw new FormatException($"Type do not match\n x: {xIsDouble}\n");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch
+                {
+                    Console.WriteLine("Unknown error!");
+                }
+            }
+            while (!xIsDouble);
+
+            Console.WriteLine($"4)cos(arctg(x)): {Math.Cos(Math.Atan(x))}");
+        }
+    }
+}
---
+++ 1.LabOOP/Lab1_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double X1 = 0.0;
+            double Y1 = 0.0;
+            bool X1IsDouble = false;
+            bool Y1IsDouble = false;
+            //Ввод и проверка на тип double переменных X1,Y1
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter X1 (double): ");
+                    X1IsDouble = Double.TryParse(Console.ReadLine(), out X1);
+                    Console.WriteLine("Enter Y1 (double): ");
+                    Y1IsDouble = Double.TryParse(Console.ReadLine(), out Y1);
+                    if (!X1IsDouble || !Y1IsDouble) throw new FormatException($"Types do not match\nX1: {X1IsDouble}\nY1: {Y1IsDouble}\n");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch
+                {
+                    Console.WriteLine("Unknown error!");
+                }
+            }
+            while (!X1IsDouble || !Y1IsDouble);
+
+            //Проверка на включение точки в обл.значений
+            bool isContained = false;
+            if((X1 >= 0.0 && X1 <= 5.0 && Y1 <= -X1 + 5 && Y1 >= 0) || (X1 >= 0 && X1 <= 5 && Y1 <= 0 && Y1 >= -7)) 
+            {
+                isContained = true;
+                Console.WriteLine($"Point with coordinates X1: {X1} Y1: {Y1} is in area");
+                Console.WriteLine(isContained);
+            }
+            else
+            {
+                Console.WriteLine($"Point with coordinates X1: {X1} Y1: {Y1} is NOT in area");
+                Console.WriteLine(isContained);
+            }
+        }
+    }
+}
