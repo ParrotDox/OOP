01e7d88ecc3eec414a6608a82c5f556586ed8465
Bibub <slappierR@gmail.com>
2024-10-30 23:41:59 +0500

    started doing lab4

---
+++ 4.LabOOP/Lab9/Money.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    internal class Point
+    {
+        //Делегат для инициализации события
+        public delegate void PointEventHandler(Point sender, PointEventArgs e);
+        public event PointEventHandler Notify;
+        public void RegisterEventNotify(PointEventHandler del) 
+        {
+            Notify = del;
+        }
+        //Объявление и инициализация полей через посредника
+        private double x {  get; set; }
+        private double X 
+        {
+            get { return x; }
+            set 
+            {
+                x = value; 
+            }
+        }
+        private double y { get; set; }
+        private double Y 
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        //Конструктор с передачей сообщения о событии
+        public Point(double x, double y) 
+        {
+            X = x;
+            Y = y;
+            Notify(this, new PointEventArgs($"Creating point->cord.:[{x}:{y}]", x, y));
+        }
+    }
+    //Класс сообщения при вызове событий в классе Point
+    internal class PointEventArgs 
+    {
+        private string msg;
+        private double x, y;
+        public PointEventArgs(string msg, double x, double y) 
+        {
+            this.msg = msg;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
