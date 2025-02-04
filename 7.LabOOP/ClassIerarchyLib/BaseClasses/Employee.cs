77a11a4b8e6c63b2c19cc00e6af38d841cb530de
ParrotDox <SlappierR@gmail.com>
2025-02-04 21:36:58 +0500

    Added PointList, not all methods done (Lab 7)

---
+++ 7.LabOOP/ClassIerarchyLib/BaseClasses/Employee.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIerarchyLib
+{
+    public class Employee : Person
+    {
+        protected static List<int> _id_storage = new List<int>();
+        private int _id;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value < 0 && !_id_storage.Contains(value))
+                {
+                    throw new ArgumentException("Id exception!");
+                }
+                _id = value;
+            }
+        }
+        private int _experience;
+        public int Experience
+        {
+            get
+            {
+                return _experience;
+            }
+            set
+            {
+                if (value < 2 || value > 84)
+                {
+                    throw new ArgumentException("Experience must be between 2 and 84");
+                }
+                _experience = value;
+            }
+        }
+        private int _salary;
+        public int Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if (value < 20000 || value > 200000)
+                {
+                    throw new ArgumentException("Salary must be between 20'000 and 200'000");
+                }
+                _salary = value;
+            }
+        }
+
+        public Employee() : base()
+        {
+            _id = -1;
+            Experience = 2;
+            Salary = 20000;
+        }
+        public Employee(string name, int age, string residence, int id, int experience, int salary) : base(name, age, residence)
+        {
+            Id = id;
+            _id_storage.Add(id);
+            Experience = experience;
+            Salary = salary;
+        }
+        public Employee(string name, int age, string residence, int id, int experience, int salary, Link link_sample) : base(name, age, residence, link_sample)
+        {
+            Id = id;
+            _id_storage.Add(Id);
+            Experience = experience;
+            Salary = salary;
+        }
+        public Employee(Employee copy_sample) : base(copy_sample)
+        {
+            Id = genId();
+            _id_storage.Add(Id);
+            Experience = copy_sample._experience;
+            Salary = copy_sample._salary;
+        }
+        //Возврат ссылки на базовый экземпляр класса (3 задание 11 лаб.)
+        public Person basePerson() 
+        {
+            return (Person)this.Clone();
+        }
+        public override void Show()
+        {
+            base.Show();
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Experience: {Experience}");
+            Console.WriteLine($"Salary: {Salary}");
+        }
+        public override string GetInfo()
+        {
+            string msg = base.GetInfo();
+            msg += $"Id: {Id}\n";
+            msg += $"Experience: {Experience}\n";
+            msg += $"Salary: {Salary}\n";
+            return msg;
+        }
+        public override void Init()
+        {
+            bool idFlag, experienceFlag, salaryFlag;
+            idFlag = false;
+            experienceFlag = false;
+            salaryFlag = false;
+
+            base.Init();
+        inputMark:
+            try
+            {
+                if (!idFlag)
+                {
+                    Console.WriteLine("Input id:");
+                    int parsedInt;
+                    idFlag = Int32.TryParse(Console.ReadLine(), out parsedInt);
+                    Id = parsedInt;
+                    idFlag = true;
+                }
+                if (!experienceFlag)
+                {
+                    Console.WriteLine("Input experience:");
+                    int parsedInt;
+                    experienceFlag = Int32.TryParse(Console.ReadLine(), out parsedInt);
+                    Experience = parsedInt;
+                    experienceFlag = true;
+                }
+                if (!salaryFlag)
+                {
+                    Console.WriteLine("Input salary:");
+                    int parsedInt;
+                    salaryFlag = Int32.TryParse(Console.ReadLine(), out parsedInt);
+                    Salary = parsedInt;
+                    salaryFlag = true;
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                goto inputMark;
+            }
+        }
+        public override void RandomInit()
+        {
+            base.RandomInit();
+            Random rnd = new Random();
+            Id = genId();
+            _id_storage.Add(Id);
+            Experience = rnd.Next(2, 85);
+            Salary = rnd.Next(20000, 200001);
+        }
+        //Сравнивает 2 объекта по содержимому, но не по ключу
+        public override bool Equals(object obj)
+        {
+            if (obj is not Employee)
+            {
+                return false;
+            }
+            Employee sample = (Employee)obj;
+            if (base.Equals(obj) &&
+                this.Name == sample.Name &&
+                this.Age == sample.Age &&
+                this.Residence == sample.Residence)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        //Заметка: клонируются уникальные поля!
+        public override object Clone()
+        {
+            Employee copy = new Employee();
+            copy.Name = this.Name;
+            copy.Age = this.Age;
+            copy.Residence = this.Residence;
+            copy.link = new Link(link.notes, link.data);
+            copy.Experience = this.Experience;
+            copy.Salary = this.Salary;
+            //id поле является уникальным, но в контексте 11 лаб.
+            //требуется найти объект по значению, а не по ссылке
+            copy._id = this.Id;
+            //key поле является уникальным, но в контексте 11 лаб.
+            //требуется найти объект по значению, а не по ссылке
+            copy.key = this.key;
+            return copy;
+        }
+        private int genId() 
+        {
+            int id = 0;
+            while (_id_storage.Contains(id)) 
+            {
+                ++id;
+            }
+            return id;
+        }
+        //Переопределение GetHashCode для формирования хэш-кода по значениям полей
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Experience, Salary, Id);
+        }
+    }
+}
---
+++ 7.LabOOP/ClassIerarchyLib/Collections/PersonList.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIerarchyLib
+{
+    public class Point
+    {
+        private Person _info;
+        public Person Info 
+        {
+            get { return _info; }
+            set
+            {
+                if (value is Person)
+                    _info = value;
+                else
+                    throw new ArgumentException("set _info: Wrong data type or nullable data");
+            }
+        }
+        private Point _link_to_next;
+        public Point Link_to_next 
+        {
+            get { return _link_to_next;  }
+            set
+            {
+                if(value is Person) 
+                {
+                    _link_to_next = value;
+                }
+                else 
+                {
+                    throw new ArgumentException("set _link_to_next: Wrong data type or nullable data");
+                }
+            }
+        }
+
+        //Constructors
+        public Point() 
+        {
+            Info = new Person();
+            Link_to_next = null;
+        }
+        public Point(Person info_sample) 
+        {
+            Info = (Person)info_sample.Clone();
+            Link_to_next = null;
+        }
+
+        //Create and return point object
+        //Empty
+        static public Point Create() 
+        {
+            Point point = new Point();
+            return point;
+        }
+        //Based on data
+        static public Point Create(Person person_sample)
+        {
+            Point point = new Point(person_sample);
+            return point;
+        }
+    }
+
+    public class PointList: ICollection<Point>
+    {
+        private Point _beg;
+        private Point _end;
+        private uint _capacity;
+        private uint Capacity 
+        {
+            set 
+            {
+                if (value == 0)
+                    throw new ArgumentException("set _capacity: can't set capacity = 0, use constructor without parameters");
+            }
+            get { return _capacity; }
+        }
+        private int _count;
+        public int Count 
+        {
+            get 
+            {
+                if (_beg == null)
+                    return 0;
+                
+                Point cur_point = _beg;
+                int ctr = 1;
+                while (cur_point != null) 
+                {
+                    cur_point = cur_point.Link_to_next;
+                    ctr++;
+                }
+                return ctr;
+            }
+        }
+        //Constructors
+        public PointList() 
+        {
+            _beg = null;
+            _end = null;
+            _capacity = 0;
+        }
+        public PointList(uint capacity) 
+        {
+            //Checking if atribute correct using setter
+            Capacity = capacity;
+
+            Point sample = Point.Create();
+            sample.Info.RandomInit();
+
+            //capacity = 1
+            if (capacity == 1) 
+            {
+                _beg = sample;
+                _end = sample;
+            }
+            else if (capacity > 1)
+            {
+                _beg = sample;
+                //Temp is used to iterate through list
+                Point cur_list_point = _beg;
+                for(int c = 1; c < capacity; c++) 
+                {
+                    while (cur_list_point.Link_to_next != null)
+                        cur_list_point = cur_list_point.Link_to_next;
+
+                    sample = Point.Create();
+                    sample.Info.RandomInit();
+
+                    cur_list_point.Link_to_next = sample;
+                }
+                _end = cur_list_point.Link_to_next;
+            }
+        }
+        public PointList(PointList pointList_sample) 
+        {
+            //User may try to copy empty collection. Using setter is incorrect
+            _capacity = pointList_sample.Capacity;
+
+            if(pointList_sample.Capacity == 0) 
+            {
+                _beg = null;
+                _end = null;
+                _capacity = 0;
+            }
+            else if(pointList_sample.Capacity == 1) 
+            {
+                Point sample = Point.Create(pointList_sample._beg.Info);
+                _beg = sample;
+                _end = sample;
+            }
+            else if(pointList_sample.Capacity > 1) 
+            {
+                Point sample = Point.Create(pointList_sample._beg.Info);
+                _beg = sample;
+                //temp is used to iterate through creating list
+                Point cur_list_point = _beg;
+                //temp_sample is used to iterate through pointList_sample
+                Point cur_sample_point = pointList_sample._beg;
+                for(int c = 1; c < Capacity; ++c)
+                {
+                    while (cur_list_point.Link_to_next != null) 
+                    {
+                        cur_list_point = cur_list_point.Link_to_next;
+                        cur_sample_point = cur_sample_point.Link_to_next;
+                    }
+
+                    sample = Point.Create(cur_sample_point.Info);
+
+                    cur_list_point.Link_to_next = sample;
+                }
+                _end = cur_list_point.Link_to_next;
+            }
+        }
+        //Create and return PointList object
+        //Empty
+        static public PointList Create() 
+        {
+            PointList pointList = new PointList();
+            return pointList;
+        }
+        //With specified length
+        static public PointList Create(uint capacity)
+        {
+            PointList pointList = new PointList(capacity);
+            return pointList;
+        }
+        
+        //ICollection methods
+        public void Add(Point sample) 
+        {
+            Point cur_point = _beg;
+            while(cur_point.Link_to_next != null)
+                cur_point = cur_point.Link_to_next;
+            cur_point.Link_to_next = sample;
+            ++Capacity;
+        }
+        public void Clear() 
+        {
+            _beg = null;
+            _end = null;
+            _capacity = 0;
+        }
+        public bool Contains(Point sample) 
+        {
+            //Checking using Equals
+            bool isEquals = false;
+            
+            Point cur_point = _beg;
+            while (cur_point.Link_to_next != null) 
+            {
+                isEquals = sample.Equals(cur_point.Info);
+                cur_point = cur_point.Link_to_next;
+            }
+
+            return isEquals;
+        }
+        public void CopyTo(Point[] array, int arrayIndex) 
+        {
+            if (array == null)
+                throw new ArgumentNullException("array argument: not initialized");
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+                throw new ArgumentOutOfRangeException("arrayIndex argument: index is out of range");
+            if(arrayIndex + Count > array.Length)
+                throw new ArgumentOutOfRangeException("copyTo: can't copy, remain array part < Collection.Count");
+            Point cur_point = _beg;
+            for (int i = 0; i < Count; ++i) 
+            {
+                array[arrayIndex + i] = cur_point;
+                cur_point = cur_point.Link_to_next;
+            }
+        }
+    }
+}
